96d4b5dfffd809c5bad4038151926624caf3a3b6
syed <umaranwar707@gmail.com>
2020-11-03 16:26:59 +0530

    Added:user value

--- src/workshop/TicTacToeGame.java
+++ src/workshop/TicTacToeGame.java
@@ -1,8 +1,9 @@
 
 package workshop;
-
+import java.util.Scanner;
 public class TicTacToeGame {
-
+ static char[] game;
+ static char comp,user;
 	public static void main(String[] args) {
 		game=new char[10];
 		System.out.println("lets play");
@@ -13,10 +14,22 @@ public class TicTacToeGame {
 			for (int i=0;i<10;i++) {
 				game[i]=' ';
 				
-				System.out.println(game [i]);
+				System.out.print(game [i]);
 				
 			}}
-			
+		public static void option() {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("enter the option");
+			char user=sc.next().charAt(0);
+			if(user=='x') {
+				comp='o';
+			}
+			else if(user=='o') {
+				comp='x';
+			}
+			else {System.out.println("enter the correct value");}
+		}
+		
 		}
 
 	
