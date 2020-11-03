5a0fd48532a099c5058c6b5c96b5d35d21459049
syed <umaranwar707@gmail.com>
2020-11-03 17:31:58 +0530

    Added:Board is displayed for user

--- src/workshop/TicTacToeGame.java
+++ src/workshop/TicTacToeGame.java
@@ -8,29 +8,44 @@ public class TicTacToeGame {
 		game=new char[10];
 		System.out.println("lets play");
 		playgame();
+		option();
+		showBoard();
 	}
 		
-		public static void playgame() {
+	public static void playgame() {
 			for (int i=0;i<10;i++) {
 				game[i]=' ';
 				
 				System.out.print(game [i]);
 				
-			}}
-		public static void option() {
+			 }
+	}
+	  public static void option() {
 			Scanner sc=new Scanner(System.in);
 			System.out.println("enter the option");
 			char user=sc.next().charAt(0);
-			if(user=='x') {
+			int count=0;
+			while (count==0) {
+			    if(user=='x'||user=='X') {
 				comp='o';
-			}
-			else if(user=='o') {
+				count++;
+			    }
+			    else if(user=='o'|| user=='O') {
 				comp='x';
-			}
-			else {System.out.println("enter the correct value");}
-		}
-		
-		}
+				count++;
+			    }
+			    else {System.out.println("enter the correct value");
+			    user=sc.next().charAt(0);
+			    }
+			} 
+		  sc.close();
+	   }
+	  public static void showBoard() {
+		  for(int i=0;i<game.length;i++) {
+			 System.out.println("|"+game[i]+"|"+game[i+1]+"|"+game[i+2]+"|"); 
+		  }
+	  }
+}	
 
 	
 
