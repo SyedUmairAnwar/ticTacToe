d50885cc0bebb6721369bb8212f9199423d31bad
syed <umaranwar707@gmail.com>
2020-11-03 15:49:24 +0530

    Added:Welcome to TicTacToe

---
+++ src/workshop/TicTacToeGame.java
@@ -0,0 +1,24 @@
+
+package workshop;
+
+public class TicTacToeGame {
+
+	public static void main(String[] args) {
+		game=new char[10];
+		System.out.println("lets play");
+		playgame();
+	}
+		
+		public static void playgame() {
+			for (int i=0;i<10;i++) {
+				game[i]=' ';
+				
+				System.out.println(game [i]);
+				
+			}}
+			
+		}
+
+	
+
+
